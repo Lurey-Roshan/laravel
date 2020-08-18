b1baf5434bde6865267c08a6e38cf53dd21e469e
Roshan Subedi <everforgoodrs@gmail.com>
2020-08-18 09:46:10 +0545

    changes made in routes and PostController

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function(){
+	return view('welcome');
+});
+Route::get('/hy', function(){
+	return "Happy Birthday";
+});
+Route::get('/hi/{id}', function($id){
+	return" this is hi from id ".$id;
+});
+Route::get('/hi/{name}', function($name){
+	return "this is hi from ".$name; 
+});
+Route::get('/hi/{name}/{id}', function($name, $id){
+	return "this is hi from ".$name." and id is ".$id;
+});
+Route::get('/admin/post/example',array('as' =>'admin.home',function(){
+	$url=route('admin.home');
+	return"this url is ".$url;
+}));
+#use of controllers in route. Learning well 
+Route::get('/post','PostController@index');
+Route::get('/post/{id}', 'PostController@show'); 
+# use of resource method  to get routes of controller
+Route::resource('/post','PostController');
